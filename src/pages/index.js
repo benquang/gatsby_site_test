9795ff585d285c8a382a59732f95dce5a860f204
benquang <thangfacebook1905@gmail.com>
2025-06-01 10:50:11 +0700

    add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -179,11 +179,16 @@ export const Head = () => <title>Home Page</title>
 
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>upload speed test</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   )
 }
