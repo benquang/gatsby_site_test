95de8ceb28966002e05081940936b831d7a31cba
benquang <thangfacebook1905@gmail.com>
2025-06-01 10:29:03 +0700

    them nguyen tri dung

--- src/pages/index.js
+++ src/pages/index.js
@@ -183,7 +183,7 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Nguyen tri dung</p>
     </Layout>
   )
 }
