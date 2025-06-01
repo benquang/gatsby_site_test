a8d60ac7888850335e89607c9cba7c6869c75fdc
benquang <thangfacebook1905@gmail.com>
2025-06-01 10:20:23 +0700

    first up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/*
 import * as React from "react"
 
 const pageStyles = {
@@ -174,3 +175,19 @@ const IndexPage = () => {
 export default IndexPage
 
 export const Head = () => <title>Home Page</title>
+*/
+
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>Home Page</title>
+
+export default IndexPage
